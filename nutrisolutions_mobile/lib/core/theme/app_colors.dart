eb49b800a6ee81411b76065059cd1d4d70256a35
Houcem Hbiri <houcem96.hh@gmail.com>
2025-04-13 16:59:22 +0100

    setup mobile project structure

---
+++ nutrisolutions_mobile/lib/core/theme/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFCC5500);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color secondaryColor = Color(0xFF9DC209);
+  static const Color backgroundColor = Color(0xFFFBF4EA);
+  static const Color brown = Color(0xFF5D2D00);
+  static const Color lightGreen = Color(0xFFCBEA7B);
+  static const Color textColor = Color(0xFF263A43);
+  static const Color hintText = Color(0xFFB1B1B1);
+  static const Color red = Color(0xFFDF0404);
+  static const Color lightRed = Color(0xFFFFC5C5);
+  static const Color lightGray = Color(0xFFF5F5F5);
+  static const Color lightOrange = Color(0xFFFBB03B);
+  static const Color starColor = Color(0xFFFFB90A);
+
+  static const BoxShadow dropShadow = BoxShadow(
+    color: Color.fromRGBO(0, 0, 0, 0.1),
+    offset: Offset(0, 7),
+    blurRadius: 10,
+  );
+}
---
+++ nutrisolutions_mobile/lib/core/theme/theme.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'app_colors.dart';
+
+final poppins = GoogleFonts.poppins(letterSpacing: 0.8);
+final dancingScript = GoogleFonts.dancingScript(letterSpacing: 0.8);
+
+ThemeData lightTheme = ThemeData(
+  textTheme: TextTheme(
+    headlineLarge: poppins.copyWith(
+        color: AppColors.primaryColor,
+        fontSize: 40,
+        fontWeight: FontWeight.w600),
+    headlineMedium: poppins.copyWith(
+        color: AppColors.secondaryColor,
+        fontSize: 22,
+        fontWeight: FontWeight.w500),
+    titleLarge: dancingScript.copyWith(
+        color: AppColors.primaryColor,
+        fontSize: 25,
+        fontWeight: FontWeight.w600),
+    titleMedium: poppins.copyWith(
+        color: AppColors.black, fontSize: 20, fontWeight: FontWeight.w600),
+    titleSmall: poppins.copyWith(
+        color: AppColors.black, fontSize: 18, fontWeight: FontWeight.w600),
+    bodySmall: poppins.copyWith(
+        color: AppColors.black, fontSize: 14, fontWeight: FontWeight.w400),
+    bodyMedium: poppins.copyWith(
+        color: AppColors.black, fontSize: 17, fontWeight: FontWeight.w500),
+    labelLarge: poppins.copyWith(
+        color: AppColors.textColor, fontSize: 20, fontWeight: FontWeight.w600),
+    labelMedium: poppins.copyWith(
+        color: AppColors.hintText, fontSize: 16, fontWeight: FontWeight.w600),
+    labelSmall: poppins.copyWith(
+        color: AppColors.white, fontSize: 14, fontWeight: FontWeight.w600),
+  ),
+  appBarTheme: const AppBarTheme(
+    color: AppColors.primaryColor,
+    elevation: 0,
+  ),
+  colorScheme: const ColorScheme.light(
+      primary: AppColors.white, background: Colors.transparent),
+  primaryColor: AppColors.primaryColor,
+  bottomNavigationBarTheme:
+      const BottomNavigationBarThemeData(backgroundColor: Colors.transparent),
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: AppColors.lightOrange),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all<Color>(AppColors.lightGreen),
+      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+          const EdgeInsets.symmetric(horizontal: 20, vertical: 15)),
+      shape: MaterialStateProperty.all<OutlinedBorder>(
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(11)),
+      ),
+      minimumSize: MaterialStateProperty.all<Size>(
+        const Size(double.infinity, 20), // Set the minimum width to infinity
+      ),
+    ),
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    contentPadding: EdgeInsets.zero,
+    border: InputBorder.none,
+    hintStyle: poppins.copyWith(
+        color: AppColors.hintText, fontSize: 16, fontWeight: FontWeight.w400),
+  ),
+  drawerTheme: DrawerThemeData(
+    backgroundColor: AppColors.brown,
+    elevation: 8,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.horizontal(right: Radius.circular(24)),
+    ),
+    scrimColor: AppColors.lightOrange.withOpacity(0.5), // when drawer slides in
+  ),
+);
